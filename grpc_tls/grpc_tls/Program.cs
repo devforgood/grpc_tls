b1ed49be5978949d6015c75ada78f7a29b9bb16f
denny.song <denny.song@friendsgames.com>
2020-10-29 11:18:40 +0900

    grpc tls

---
+++ grpc_tls/grpc_tls/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace grpc_tls
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
